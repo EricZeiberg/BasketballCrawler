9a983e6ebe57de8db254a82376c073e8afa906f3
Eric Zeiberg <ejzeiberg@gmail.com>
2015-12-26 17:53:08 -0500

    Refactor a couple of things

--- src/com/ericzeiberg/basketballcrawler/Crawler.java
+++ src/com/ericzeiberg/basketballcrawler/Crawler.java
@@ -2,6 +2,8 @@ package com.ericzeiberg.basketballcrawler;
 
 import com.ericzeiberg.basketballcrawler.models.Game;
 import com.ericzeiberg.basketballcrawler.models.Team;
+import com.ericzeiberg.basketballcrawler.utils.MiscUtils;
+import com.sun.tools.hat.internal.util.Misc;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -30,7 +32,7 @@ public class Crawler {
                 }
                 Element aTag = e.getElementsByTag("td").first().getElementsByTag("font").first().getElementsByTag("a").first();
                 Team t = new Team(aTag.text());
-                System.out.println("Adding new team " + t.getName());
+                //System.out.println("Adding new team " + t.getName());
                 teams.add(t);
                 parseTeamPage(t, aTag.attr("href"));
                 i++;
@@ -51,29 +53,41 @@ public class Crawler {
                     i++;
                     continue;
                 }
+                if (e.getElementsByTag("td").size() < 4){
+                    continue;
+                }
                 String date = e.getElementsByTag("td").first().text();
                 Team newTeam = new Team(e.getElementsByTag("td").get(1).text());
-                if (searchTeams(newTeam) != null){
-                    newTeam = searchTeams(newTeam);
+
+                if (MiscUtils.searchTeams(newTeam, teams) != null){
+                    newTeam = MiscUtils.searchTeams(newTeam, teams);
                 }
-                String location = e.getElementsByTag("td").get(2).text().split("-")[1];
                 String points = e.getElementsByTag("td").get(3).text();
                 if (points.contains("p.m.") || points.contains("a.m. ") || points.contains("TBA")){
                     continue;
                 }
+                String location;
+                try {
+                     location = e.getElementsByTag("td").get(2).text().split("-")[1];
+                }
+                catch (ArrayIndexOutOfBoundsException e1){
+                    location = "N/A";
+                }
                 Game g;
-                if (e.getElementsByTag("td").get(3).text().split("-")[1].equalsIgnoreCase("Home")){
-                    if (points.split(" ")[0].equalsIgnoreCase("W")){
-                        g = new Game(t, newTeam, location, date, Integer.parseInt(points.split(" ")[1].split("-")[0]), Integer.parseInt(points.split(" ")[1].split("-")[1].replace("\u00a0","")), true);
-                        if (searchGames(g) == null){
+                if (e.getElementsByTag("td").get(2).text().split("-")[0].contains("Home")){
+                    if (points.split(" ")[0].contains("W")){
+                        String[] pointArray = MiscUtils.cleanString(points.split(" ")[1].split("-"));
+                        g = new Game(t, newTeam, location, date, Integer.parseInt(pointArray[0]), Integer.parseInt(pointArray[1]), true);
+                        if (MiscUtils.searchGames(g, games) == null){
                             games.add(g);
                         }
                         t.setWins(t.getWins() + 1);
                         newTeam.setLosses(newTeam.getLosses() + 1);
                     }
                     else {
-                        g = new Game(t, newTeam, location, date, Integer.parseInt(points.split(" ")[1].split("-")[0]), Integer.parseInt(points.split(" ")[1].split("-")[1].replace("\u00a0","")), false);
-                        if (searchGames(g) == null){
+                        String[] pointArray = MiscUtils.cleanString(points.split(" ")[1].split("-"));
+                        g = new Game(t, newTeam, location, date, Integer.parseInt(pointArray[0]), Integer.parseInt(pointArray[1]), false);
+                        if (MiscUtils.searchGames(g, games) == null){
                             games.add(g);
                         }
                         t.setLosses(t.getLosses() + 1);
@@ -81,52 +95,46 @@ public class Crawler {
                     }
                 }
                 else {
-                    if (points.split(" ")[0].equalsIgnoreCase("W")){
-                        g = new Game(newTeam, t, location, date, Integer.parseInt(points.split(" ")[1].split("-")[0]), Integer.parseInt(points.split(" ")[1].split("-")[1].replace("\u00a0","")), false);
-                        if (searchGames(g) == null){
+                    if (points.split(" ")[0].contains("W")){
+                        String[] pointArray = MiscUtils.cleanString(points.split(" ")[1].split("-"));
+                        g = new Game(newTeam, t, location, date, Integer.parseInt(pointArray[0]), Integer.parseInt(pointArray[1]), false);
+                        if (MiscUtils.searchGames(g, games) == null){
                             games.add(g);
                         }
                         t.setWins(t.getWins() + 1);
                         newTeam.setLosses(newTeam.getLosses() + 1);
                     }
                     else {
-                        g = new Game(newTeam, t, location, date, Integer.parseInt(points.split(" ")[1].split("-")[0]), Integer.parseInt(points.split(" ")[1].split("-")[1].replace("\u00a0","")), true);
-                        if (searchGames(g) == null){
+                        String[] pointArray = MiscUtils.cleanString(points.split(" ")[1].split("-"));
+                        g = new Game(newTeam, t, location, date, Integer.parseInt(pointArray[0]), Integer.parseInt(pointArray[1]), true);
+                        if (MiscUtils.searchGames(g, games) == null){
                             games.add(g);
                         }
                         t.setLosses(t.getLosses() + 1);
                         newTeam.setWins(newTeam.getWins() + 1);
                     }
                 }
-                System.out.println(g);
+                //System.out.println(g);
                 System.out.println(newTeam);
-                if (searchTeams(newTeam) == null){
+                if (MiscUtils.searchTeams(newTeam, teams) != null){
+                    MiscUtils.replaceTeam(newTeam, teams);
+                }
+
+                if (MiscUtils.searchTeams(newTeam, teams) == null){
                     teams.add(newTeam);
                 }
                 i++;
             }
-        } catch (IOException e) {
+        } catch (IOException | ArrayIndexOutOfBoundsException e) {
             e.printStackTrace();
         }
 
-
-    }
-
-    private static Team searchTeams(Team t){
-        for (Team e : teams){
-            if (t.getName().equalsIgnoreCase(e.getName())){
-                return e;
-            }
+        System.out.println();
+        for (Team t1 : teams){
+            System.out.println(t1);
         }
-        return null;
+
     }
 
-    private static Game searchGames(Game g){
-        for (Game a : games){
-            if (g.isSameGame(a)){
-                return a;
-            }
-        }
-        return null;
-    }
+
 }
